f96e387200bba20d290f4a708cbbb24d2000a2c3
Ellis Osborn <ejosborn@asu.edu>
2023-11-16 11:17:40 -0800

    changed path (put inside api folder)

---
+++ API/README.md
@@ -0,0 +1,42 @@
+# Token-Based Authentication System API
+This project is the API for the Token-Based Authentication System. It will use Python and Flask for the backend, AngularJS and Javascript for the frontend, and SQLite as the database. 
+
+## Getting Started
+
+### Prerequisites
+
+### Installing
+- Install Flask
+```python
+pip install flask
+```
+
+- Install Flask_JWT_Extended
+```python
+pip install flask_jwt_extended
+```
+
+- (Optional) Install dotenv
+```python
+pip install python-dotenv
+```
+
+- Install SQLITE3 if you have Python version 2.7.3 or older (already included if you use Python 3.X.X)
+```python
+pip install pysqlite
+```
+
+### Libraries Used
+
+- [Flask](https://flask.palletsprojects.com/en/3.0.x/) - Used to host the backend framework
+- [Flask_JWT_Extended](https://flask-jwt-extended.readthedocs.io/en/stable/) - Used for making JSON Web Tokens for only taking in valid requests
+- dotenv - Reading .env variables
+- [SQLite3](https://www.sqlite.org/index.html) - Storing user data
+### Usage
+
+### Author
+* **Ellis Osborn** - [Ejosborn](https://github.com/ejosborn)
+
+## Project Status
+
+- I am currently working on this project, so code will be committed often.
---
+++ API/authorize.py
@@ -0,0 +1,86 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import sqlite3
+
+load_dotenv()
+app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = os.getenv('SECRET_KEY')
+jwt = JWTManager(app)
+
+# creating database file
+connect = sqlite3.connect('users.db')
+cursor = connect.cursor()
+
+# creating users table
+cursor.execute(
+    ''' CREATE TABLE IF NOT EXISTS users 
+    (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+    )'''
+)
+
+connect.commit()
+connect.close()
+
+# api endpoint for registering users
+@app.route("/register", methods=["POST"])
+def register():
+    info = request.get_json()
+    username = info.get('username')
+    password = info.get('password')
+
+    #check if exists in user table
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT username FROM users WHERE username = ?', (username,))
+    exists = cursor.fetchone()
+    conn.close()
+
+    #if username exists
+    if exists:
+        return jsonify({'error': 'Username is already taken. Choose a unique username.'}), 400
+    
+    #create user
+    conn.sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO users (username, password) VALUES (?,?)' (username, password))
+    conn.commit()
+    conn.close()
+
+    return jsonify({'message': 'User Registration Successful!'}), 201
+
+
+# api endpoint for logging users in
+@app.route("/login", methods=["POST"])
+def login():
+    info = request.get_json()
+    username = info.get('username')
+    password = info.get('password')
+
+    #check user and password vs database data
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+    userExists = cursor.fetchone()
+
+    #creates valid jwt if user exists, else return error
+    if userExists:
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({'error': 'Invalid username or password'}), 401
+
+# api endpoint making sure requests have valid JWT Web Tokens
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity
+    return jsonify(logged_in=current_user), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
